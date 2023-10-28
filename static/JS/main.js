730c5df09b8b07140a72a068e704c6da15d37c7c
PyroGuy <damirakhm@gmail.com>
2023-10-28 11:07:49 +0300

    cool_stuff

--- static/JS/main.js
+++ static/JS/main.js
@@ -8,3 +8,16 @@ button.onclick = function() {
         behavior: 'smooth'
     });
 }
+
+// Слайдер
+var cssElement = document.querySelector('.images_slaider');
+var stule = window.getComputedStyle(cssElement);
+setInterval(function(){
+    if(stule.transform == 'none'){
+        cssElement.style.cssText = "transform: translateX(-100%)";
+    } else if(stule.transform[20] == '3'){
+        cssElement.style.cssText = "transform: translateX(-200%)";
+    } else{
+        cssElement.style.cssText = "transform: none";
+    }
+}, 2000)
