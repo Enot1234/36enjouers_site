f828c12457a6bfb8962e790171890bdb24604400
Enot1234 <130364288+Enot1234@users.noreply.github.com>
2023-10-28 18:58:33 +0500

    1

--- userpanel/views.py
+++ userpanel/views.py
@@ -5,4 +5,4 @@ def lichniy_kabinet(request):
     tests = Test.objects.all()
     for i in tests:
         print(i.title)
-    return render(request, "lichniy_kabinet.html", {"tests":tests})
+    return render(request, "ofice/lichniy_kabinet.html", {"tests": tests})
