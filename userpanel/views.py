25a3ba5d66c2ec3e3c01aaca2b1f3d3281a2a981
Enot1234 <130364288+Enot1234@users.noreply.github.com>
2023-10-28 14:09:37 +0500

    Update views.py

--- userpanel/views.py
+++ userpanel/views.py
@@ -1,8 +1,4 @@
 from django.shortcuts import render
 
-<<<<<<< HEAD
-# Create your views here.
-=======
 def lichniy_kabinet(request):
     return render(request, "lichniy_kabinet.html")
->>>>>>> 9e1b61d678cd4e8df13e7d73e3c2cf770a72b948
