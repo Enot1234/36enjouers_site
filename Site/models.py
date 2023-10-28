808be6172ecd0f903d36430719747a1136c99e33
Enot1234 <130364288+Enot1234@users.noreply.github.com>
2023-10-28 10:51:07 +0500

    model

--- Site/models.py
+++ Site/models.py
@@ -2,4 +2,16 @@ from django.db import models
 
 
 class Schools(models.Model):
-    pass
+    FIO = models.TextField("ФИО")
+    tel = models.IntegerField("Телефон", max_length=8)
+    email = models.EmailField("email")
+    INN = models.PositiveIntegerField("Телефон", max_length=10)
+    name = models.CharField("Название организации", max_length=50)
+    adress = models.TextField("Адрес организации")
+
+    class Meta:
+        verbose_name = "Организация"
+        verbose_name_plural = "Организация"
+
+    def __str__(self):
+        return self.name
