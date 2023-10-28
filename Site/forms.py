25ce71c2089f768d600909298e29bf1c65fc1e7a
Enot1234 <130364288+Enot1234@users.noreply.github.com>
2023-10-28 17:24:36 +0500

    1

--- Site/forms.py
+++ Site/forms.py
@@ -1,2 +1,9 @@
-from django.contrib.auth.models import User
+from django import forms
+from .models import school
 
+
+class RegSchool(forms.ModelForm):
+    pass
+
+    # class Meta:
+    #     model = school
