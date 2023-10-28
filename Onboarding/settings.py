f2a4aeaec23eb63d4bcd0768e733cc10de77b561
cpu-err <mag.34737@gmail.com>
2023-10-28 11:55:38 +0500

    userpanel

--- Onboarding/settings.py
+++ Onboarding/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "userpanel"
 ]
 
 ROOT_URLCONF = "Onboarding.urls"
