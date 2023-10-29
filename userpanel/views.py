21055313b120e7a5bdd7a60ea641929952f52968
cpu-err <mag.34737@gmail.com>
2023-10-29 14:43:58 +0500

    админка

--- userpanel/views.py
+++ userpanel/views.py
@@ -2,17 +2,22 @@ from django.shortcuts import render
 from .models import Study
 from .models import Result
 from .models import Test
+from Site.models import school
 from django.contrib.auth.decorators import login_required
 
 @login_required
 def lichniy_kabinet(request):
-    tests = Test.objects.filter(user=request.user)
-    results = Result.objects.filter(user=request.user)
-    courses = Study.objects.filter(user=request.user)
     usr = request.user
+    tests = Test.objects.filter(user=usr)
+    results = Result.objects.filter(user=usr)
+    courses = Study.objects.filter(user=usr)
 
     return render(request, "office/PersonalArea.html", {"tests": tests, "results": results, "courses": courses, "user": usr})
 
 @login_required
 def adminarea(request):
-    return render(request, "office/PersonalAdminArea.html")
+    usr = request.user
+    org = school.objects.filter(user=usr)
+    orgname = org[0].name
+
+    return render(request, "office/PersonalAdminArea.html", {"orgname": orgname, "user": usr})
