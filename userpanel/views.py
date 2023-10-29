18d171f92a50210a73ee65dd5a92f436ea7b2ba2
cpu-err <mag.34737@gmail.com>
2023-10-29 11:27:19 +0500

    Update views.py

--- userpanel/views.py
+++ userpanel/views.py
@@ -3,11 +3,12 @@ from .models import Study
 from .models import Result
 from .models import Test
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
 @login_required
 def lichniy_kabinet(request):
-    tests = Test.objects.all()
-    results = Result.objects.all()
-    courses = Study.objects.all()
+    tests = Test.objects.filter(user=request.user)
+    results = Result.objects.filter(user=request.user)
+    courses = Study.objects.filter(user=request.user)
 
     return render(request, "office/lichniy_kabinet.html", {"tests": tests, "results": results, "courses": courses})
